29c80b64ef744c3e4be08580ddb34b3d92d4ee3e
Fabian Barajas <fbarajas@nvidia.com>
2025-08-14 17:30:01 -0700

    build(package): stop including license in extension zips

--- package.sh
+++ package.sh
@@ -7,11 +7,11 @@ mkdir -p dist
 
 # Package the Chrome extension
 echo "Packaging Chrome extension..."
-(cd grepl-chrome && zip -r -FS ../dist/grepl-chrome.zip . ../LICENSE)
+(cd grepl-chrome && zip -r -FS ../dist/grepl-chrome.zip .)
 
 # Package the Firefox extension
 echo "Packaging Firefox extension..."
-(cd grepl-firefox && zip -r -FS ../dist/grepl-firefox.zip . ../LICENSE)
+(cd grepl-firefox && zip -r -FS ../dist/grepl-firefox.zip .)
 
 echo "Packaging complete."
 echo "Files are located in the 'dist' directory."
